3c5a343ed0d15b1aeac01ede845bfc6d4ffb9236
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2024-01-29 16:52:09 +0530

    fix:minor fixis

--- backend/model/Category.js
+++ backend/model/Category.js
@@ -22,4 +22,4 @@ const categorySchema = new mongoose.Schema({
     },
 });
 
-module.exports = mongoose.model('category', categorySchema)
+module.exports = mongoose.model('Category', categorySchema)
