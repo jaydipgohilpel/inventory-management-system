126000105fbe3056f4a93eb26a5d9872076d3245
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2024-01-31 11:42:40 +0530

    feat:add customer api,test bal...

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoryRouter = require('./routes/category');
 var productRouter = require('./routes/product');
+var customerRouter = require('./routes/customer');
 
 var app = express();
 
@@ -40,6 +41,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/category', categoryRouter);
 app.use('/product', productRouter);
+app.use('/customer', customerRouter);
 
 // throw 404 if URL not found
 app.all("*", function (req, res) {
