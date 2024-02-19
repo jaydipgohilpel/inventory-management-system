623a6dc4f8878f80d71dd482788b1cf9b9062acb
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2024-02-19 16:51:13 +0530

    frontend changes , origin changes

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const apiResponse = require("./helpers/apiResponse");
 const cors = require('cors');
+require('dotenv').config();
+
 
 require('./config/config');
 require('mongoose')
