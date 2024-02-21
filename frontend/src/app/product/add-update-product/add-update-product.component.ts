135adb68eea951a44296db31b444b09340480561
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2024-02-21 14:53:17 +0530

    changes in product page

--- frontend/src/app/product/add-update-product/add-update-product.component.ts
+++ frontend/src/app/product/add-update-product/add-update-product.component.ts
@@ -52,6 +52,7 @@ export class AddUpdateProductComponent {
   }
 
   onSaveClick(): void {
+    if (this.productForm.invalid) return
     this.data = {
       ...this.data,
       form: this.productForm,
