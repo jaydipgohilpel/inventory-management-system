151939004f8760b6373f8b4194fedba3289992f5
jaydipgohilpel <jaydip.gohil@proexelancers.com>
2024-02-21 11:53:02 +0530

    Revert "merge backend main to dev"
    This reverts commit 13a794c8450aad89003c26ef1be03f85a82cf156, reversing
    changes made to 5fd27161c5352e4398b5da5efa61a5f8cc9f08af.

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 // mongoose.connect(process.env.MONGODB_CONNECTION)
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB', err));
